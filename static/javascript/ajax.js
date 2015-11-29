b172e3b5da7f59c20f8c5104553abedae4852a0d
Sam <sfried8@gmail.com>
2015-11-29 16:09:02 -0500

    added jquery

--- static/javascript/ajax.js
+++ static/javascript/ajax.js
@@ -2,9 +2,10 @@ $(document).ready(function() {
 
     $('#active').click(function(){
     var scroll;
+    alert("Success?");
     scroll = $(this).attr("data-scroll");
-     $.get('/update_active/', {brother_scroll:scroll, brother_active:$('#active').checked}, function(){
-               alert("Success?");
+    $.get('/update_active/', {brother_scroll:scroll, brother_active:$('#active').checked}, function(){
+               alert("Success!");
            });
 });
 
